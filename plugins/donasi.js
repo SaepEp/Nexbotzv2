14ddca102469dc9c7fb01a9f7da20f488b1ec6a6
SaepEp <skedebes601@gmail.com>
2022-12-25 08:18:37 +0700

    Nexbotzv2

---
+++ plugins/donasi.js
@@ -0,0 +1,33 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `
+┌─「 Donasi • Pulsa 」
+│ • *Telkomsel:* [${global.ppulsa}]
+│ • *Telkomsel:* [${global.ppulsa2}]
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+│ • *Dana:* [${global.pdana}]
+❏────
+`
+const templateButtons = [
+    {index: 1, urlButton: {displayText: '✨ Saweria', url: psaweria}},
+    {index: 2, urlButton: {displayText: '📷 Instagram', url: sig}},
+    {index: 3, urlButton: {displayText: '🌎 Official Group', url: sgc}},
+    {index: 4, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
+    {index: 5, quickReplyButton: {displayText: 'Owner', id: '.owner'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: {url: fla + 'Donasi'}
+}
+conn.sendMessage(m.chat, tm, m)
+}
+handler.command = /^(donasi|dns)$/i
+handler.tags = ['info']
+handler.help = ['donasi']
+handler.premium = false
+handler.limit = false
+
+export default handler
